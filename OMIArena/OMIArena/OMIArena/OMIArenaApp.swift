03fa27470e1ce2dd36f5daf4d930f91151644e0d
Ethan Lai <egl.ethanlai@gmail.com>
2025-11-22 13:20:55 -0800

    added OMIArena - XCode

---
+++ OMIArena/OMIArena/OMIArena/OMIArenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMIArenaApp.swift
+//  OMIArena
+//
+//  Created by Ethan Lai on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct OMIArenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
